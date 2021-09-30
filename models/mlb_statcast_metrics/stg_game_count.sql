b32c8b53baf1d0aeb60d7efd98503217f97c3702
Sat Daya Khalsa <satdayasingh@gmail.com>
2021-09-30 16:12:15 -0700

    modified stg_plate_appearance to include total games per season

--- models/mlb_statcast_metrics/stg_game_count.sql
+++ models/mlb_statcast_metrics/stg_game_count.sql
@@ -5,14 +5,17 @@
     )
 }}
 
-WITH cte_home_game AS (
+WITH cte_base_statcast AS (
+  SELECT * FROM {{ref('base_statcast')}}
+),
+cte_home_game AS (
   SELECT
     DISTINCT game_pk AS game_pk
    ,home_team AS team
    ,away_team AS opponent
    ,gm_date
    ,game_year
-  FROM base_statcast
+  FROM cte_base_statcast
   ),
 cte_away_game AS (
   SELECT
@@ -21,7 +24,7 @@ cte_away_game AS (
    ,home_team AS opponent
    ,gm_date
    ,game_year
-  FROM base_statcast
+  FROM cte_base_statcast
   ),
 cte_stack AS (
   SELECT * 
@@ -37,8 +40,29 @@ cte_count AS (
     *
    ,COUNT(game_pk) OVER (PARTITION BY team, game_year ORDER BY gm_date) AS game_count
   FROM cte_stack
+  ),
+cte_yearly_count AS (
+  SELECT
+    game_year
+   ,team
+   ,MAX(game_count) AS yearly_game_count
+  FROM cte_count
+  GROUP BY 1,2
+  ),
+cte_final AS (
+SELECT
+   c.game_pk
+  ,c.team
+  ,c.opponent
+  ,c.gm_date
+  ,c.game_year
+  ,c.game_count
+  ,y.yearly_game_count
+  ,c.game_pk || c.team as comp_key
+FROM cte_count c
+JOIN cte_yearly_count y
+  ON c.game_year = y.game_year
+ AND c.team = y.team
   )
   
-SELECT *
-  ,game_pk || team as comp_key
-FROM cte_count
+SELECT * FROM cte_final
