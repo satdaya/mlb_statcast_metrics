b7d31047845fa7d88b4ab74afad50b5671b0cea0
Sat Daya Khalsa <satdayasingh@gmail.com>
2021-10-16 22:27:01 -0700

    added distinct_pitch_sequence model

---
+++ models/mlb_statcast_metrics/analysis/distinct_pitch_sequence.sql
@@ -0,0 +1,36 @@
+{{
+    config(
+      materialized= 'table',
+      unique_key= 'dist_pitch_seq'
+    )
+}}
+
+WITH cte_pitch_sequence AS (
+  SELECT * FROM {{ref('pitch_sequences')}}
+),
+cte_distinct_pitch_sequence AS (
+  SELECT
+    pitch_sequence AS dist_pitch_seq
+    ,COUNT (dist_pitch_seq) AS sum_sequence_used
+    ,CASE WHEN SUM(is_at_bat_bool) = 0
+          THEN NULL
+          ELSE ROUND( SUM(ab_safe_or_out_bool) / SUM(is_at_bat_bool) , 3) 
+          END AS batting_avg
+    ,CASE WHEN SUM(is_plate_appearance_bool) = 0
+          THEN NULL
+          ELSE ROUND( SUM(pa_safe_or_out_bool) / SUM(is_plate_appearance_bool), 3)
+          END AS obp --on base percentage
+    ,CASE WHEN SUM(is_at_bat_bool) = 0
+          THEN NULL
+          ELSE ROUND( SUM(bases_for_slg) / SUM(is_at_bat_bool), 3)
+          END AS slg_percentage
+    ,obp + slg_percentage AS ops
+   FROM cte_pitch_sequence
+  GROUP BY 1
+  ORDER BY 2 DESC
+  ),
+cte_final AS ( 
+  SELECT * FROM cte_distinct_pitch_sequence
+)
+
+SELECT * FROM cte_final
