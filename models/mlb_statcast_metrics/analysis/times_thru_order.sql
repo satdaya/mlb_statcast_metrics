00526c072cbefc10ae4d7dc61057e06faef2ae3f
Sat Daya Khalsa <satdayasingh@gmail.com>
2021-11-10 18:40:16 -0800

    debugged times_thru_order table

--- models/mlb_statcast_metrics/analysis/times_thru_order.sql
+++ models/mlb_statcast_metrics/analysis/times_thru_order.sql
@@ -15,11 +15,11 @@ cte_remove_individual_pitchers AS (
     ,AVG(fb_velo) AS fb_velo
     ,AVG(fb_spin_rate) AS fb_spin_rate
     ,AVG(fb_x_axis_movement) AS fb_x_axis_movement
-    ,AVG(fb_z_axis_movement) AS fb_y_axis_movement
+    ,AVG(fb_z_axis_movement) AS fb_z_axis_movement
     ,AVG(br_velo) AS br_velo
     ,AVG(br_spin_rate) AS br_spin_rate
     ,AVG(br_x_axis_movement) AS br_x_axis_movement
-    ,AVG(br_z_axis_movement) AS br_y_axis_movement
+    ,AVG(br_z_axis_movement) AS br_z_axis_movement
   FROM cte_pitchers_time_thru_order
   GROUP BY 1,2
   ),
@@ -28,16 +28,16 @@ cte_consolidation AS (
      _year
     ,times_thru_order
     ,_year || times_thru_order AS tab_pk
-    ,MAX(fb_velo)
-    ,MAX(fb_spin_rate)
-    ,MAX(fb_x_axis_movement)
-    ,MAX(fb_y_axis_movement)
-    ,MAX(br_velo)
-    ,MAX(br_spin_rate)
-    ,MAX(br_x_axis_movement)
-    ,MAX(br_y_axis_movement)
+    ,MAX(fb_velo) AS fb_velo
+    ,MAX(fb_spin_rate) AS fb_spin_rate
+    ,MAX(fb_x_axis_movement) AS fb_x_axis_movement
+    ,MAX(fb_z_axis_movement) AS fb_z_axis_movement
+    ,MAX(br_velo) AS br_velo
+    ,MAX(br_spin_rate) AS br_spin_rate
+    ,MAX(br_x_axis_movement) AS br_x_axis_movement
+    ,MAX(br_z_axis_movement) AS br_z_axis_movement
   FROM cte_remove_individual_pitchers
-  GROUP BY 1,2,3
+  GROUP BY 1,2
   ORDER BY 1,2
 ),
 cte_final AS (
