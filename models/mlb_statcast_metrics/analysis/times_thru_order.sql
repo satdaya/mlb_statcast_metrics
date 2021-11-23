a452fa854014ea84c81be6bfaf5fe179edddd9fd
Sat Daya Khalsa <satdayasingh@gmail.com>
2021-11-23 12:01:25 -0800

    remove metrics from time_thru_order model

--- models/mlb_statcast_metrics/analysis/times_thru_order.sql
+++ models/mlb_statcast_metrics/analysis/times_thru_order.sql
@@ -47,33 +47,12 @@ cte_variance AS (
     ,times_thru_order
     ,_year || times_thru_order AS tab_pk
     ,fb_velo
-    ,fb_velo - LAG(fb_velo, 1) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_fb_velo_from_1st_time_thru_order
-    ,fb_velo - LAG(fb_velo, 2) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_fb_velo_from_2nd_time_thru_order
-    ,fb_velo - LAG(fb_velo, 3) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_fb_velo_from_3rd_time_thru_order
     ,fb_spin_rate
-    ,fb_spin_rate - LAG(fb_spin_rate, 1) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_fb_spin_rate_from_1st_time_thru_order
-    ,fb_spin_rate - LAG(fb_spin_rate, 2) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_fb_spin_rate_from_2nd_time_thru_order
-    ,fb_spin_rate - LAG(fb_spin_rate, 3) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_fb_spin_rate_from_3rd_time_thru_order
     ,fb_x_axis_movement
-    ,fb_x_axis_movement - LAG(fb_x_axis_movement, 1) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_fb_x_axis_from_1st_time_thru_order
-    ,fb_x_axis_movement - LAG(fb_x_axis_movement, 2) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_fb_x_axis_from_2nd_time_thru_order
-    ,fb_x_axis_movement - LAG(fb_x_axis_movement, 3) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_fb_x_axis_from_3rd_time_thru_order
     ,fb_z_axis_movement
-    ,fb_z_axis_movement - LAG(fb_z_axis_movement, 1) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_fb_z_axis_from_1st_time_thru_order
-    ,fb_z_axis_movement - LAG(fb_z_axis_movement, 2) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_fb_z_axis_from_2nd_time_thru_order
-    ,fb_z_axis_movement - LAG(fb_z_axis_movement, 3) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_fb_z_axis_from_3rd_time_thru_order
     ,br_spin_rate
-    ,br_spin_rate - LAG(br_spin_rate, 1) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_br_spin_rate_from_1st_time_thru_order
-    ,br_spin_rate - LAG(br_spin_rate, 2) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_br_spin_rate_from_2nd_time_thru_order
-    ,br_spin_rate - LAG(br_spin_rate, 3) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_br_spin_rate_from_3rd_time_thru_order
     ,br_x_axis_movement
-    ,br_x_axis_movement - LAG(br_x_axis_movement, 1) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_br_x_axis_from_1st_time_thru_order
-    ,br_x_axis_movement - LAG(br_x_axis_movement, 2) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_br_x_axis_from_2nd_time_thru_order
-    ,br_x_axis_movement - LAG(br_x_axis_movement, 3) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_br_x_axis_from_3rd_time_thru_order
     ,br_z_axis_movement
-    ,br_z_axis_movement - LAG(br_z_axis_movement, 1) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_br_z_axis_from_1st_time_thru_order
-    ,br_z_axis_movement - LAG(br_z_axis_movement, 2) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_br_z_axis_from_2nd_time_thru_order
-    ,br_z_axis_movement - LAG(br_z_axis_movement, 3) OVER (PARTITION BY _year ORDER BY times_thru_order) AS var_br_z_axis_from_3rd_time_thru_order
    FROM cte_consolidation
    ORDER BY 3
 ),
