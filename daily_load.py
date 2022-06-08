283ef5d385334022748fbd43a8536178e7ca9e03
Sat Daya Khalsa <satdayasingh@gmail.com>
2022-06-07 21:26:41 -0700

    Update daily_load.py

--- daily_load.py
+++ daily_load.py
@@ -7,8 +7,8 @@ import os
 import boto3
 
 session = boto3.Session(
-aws_access_key_id=  os.environ.get(AWS_ACCESS_KEY_ID),
-aws_secret_access_key= os.environ.get(AWS_SECRET_ACCESS_KEY)
+aws_access_key_id=  os.environ.get("AWS_ACCESS_KEY_ID"),
+aws_secret_access_key= os.environ.get("AWS_SECRET_ACCESS_KEY")
 )
 
 today = date.today()
